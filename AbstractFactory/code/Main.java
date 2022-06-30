c5342ed0e1a6af715f9dd89c2f614603da0ee929
Ananya Hegde <54988307+Ananya2504@users.noreply.github.com>
2022-06-30 05:38:02 +0530

    Create Main.java

---
+++ AbstractFactory/code/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String args[]) {
+		Male m=new Male();
+		Female fm=new Female();
+		m.maleAbledFactory();
+		m.maleDiffAbledFactory();
+		fm.femaleAbledFactory();
+		fm.femaleDiffAbledFactory();
+	}
+}
